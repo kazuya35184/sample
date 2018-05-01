e182b1b2e1fe90792c095197f0ab704bcf6dd925
kazuya matsumoto <km35184@gmail.com>
2018-05-02 00:36:59 +0900

    ああああああああ

--- resources/views/memo/ichiran.blade.php
+++ resources/views/memo/ichiran.blade.php
@@ -12,10 +12,13 @@
 
 @section('content')
   @foreach($data as $item)
+
   <table cellspacing=10px;>
     <tr>
-      <td>{{$item->title}}</td><td><a href="{{action('MemoController@edit',['id'=>$item->id])}}">詳細</td>
+      <td>{{$item->title}}</td>
+      <td><a href="{{action('MemoController@edit',['id'=>$item->id])}}">詳細</td>
     </tr>
   </table>
+  
   @endforeach
 @endsection
